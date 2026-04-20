523137da79a07984922498580f4c8607a17e10fd
Ishant Yadav <ishant3010y@gmail.com>
2026-04-20 19:13:38 +0530

    first commit

---
+++ Day0/function.java
@@ -0,0 +1,17 @@
+package Day0;
+
+public class function {
+    public static void ishant(){
+        Riyanshi();
+        System.out.println("Yashu");
+    }
+    public static void main(String[] args) {
+        System.out.println("Roshan");
+        ishant();
+    }
+
+    public static void Riyanshi(){
+        System.out.println("Ashish");
+    }
+
+}
---
+++ Day0/methods.java
@@ -0,0 +1,14 @@
+package Day0;
+public class methods{
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(100));
+        System.out.println(Math.cbrt(100));
+        System.out.println(Math.abs(-65));
+        System.out.println(Math.floor(-7.8));
+        System.out.println(Math.min(3,9));
+        System.out.println(Math.max(3,9));
+        System.out.println(Math.max(Math.max(2,22),222));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.pow(0,0));
+    }
+}
