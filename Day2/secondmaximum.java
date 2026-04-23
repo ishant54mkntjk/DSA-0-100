93b009ee3446884a20e39246b7c1fcdbcbdafcf8
Ishant Yadav <ishant3010y@gmail.com>
2026-04-23 14:13:15 +0530

    Second maximum

--- Day2/secondmaximum.java
+++ Day2/secondmaximum.java
@@ -12,20 +12,30 @@ public class secondmaximum {
         }
         int max = Integer.MIN_VALUE;
         int secondmax = Integer.MIN_VALUE;
+        boolean found =false;
         for(int i=0;i<n;i++){
             if(x[i]>max){
                 secondmax = max;
                 max = x[i];
+                
             }
-            else if(x[i]>secondmax && x[i]!=max){
-                secondmax = x[i];
+            else if( x[i]>secondmax && x[i]!=max){
+                secondmax =x[i];
+            found =true;
             }
         }
-        if(secondmax==Integer.MIN_VALUE){
-            System.out.println("No second maximum element found");
+        // if(found){
+        // System.out.println("Second maximum is: "+secondmax);
+          
+        // }
+        // else{
+        // System.out.println("Second maximum is not found");
+        // }
+        if(secondmax == Integer.MIN_VALUE){
+            System.out.println("Not Found");
         }
         else{
-            System.out.println("Second maximum element is: "+secondmax);
+            System.out.println("Found "+secondmax);
         }
     }
     
