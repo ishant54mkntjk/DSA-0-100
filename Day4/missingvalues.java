b02e43b3c43304433691e25f605fa5f11eb47703
Ishant Yadav <ishant3010y@gmail.com>
2026-04-24 19:55:52 +0530

    segregate0sand1s

--- Day4/missingvalues.java
+++ Day4/missingvalues.java
@@ -3,12 +3,12 @@ package Day4;
 public class missingvalues {
     public static void main(String[] args) {
         int[] arr= {1,2,5,4,6};
-        int n = arr.length+1;
-        int sum =n*(n+1)/2;
-        int arraysum =0;
+        long n = arr.length+1;
+        long sum =n*(n+1)/2;
+        long arraysum =0;
         for(int ele :arr){
             arraysum += ele;
         }
-        System.out.print("Missing value is:"+(sum - arraysum));
+        System.out.print("Missing value is:"+(int)(sum - arraysum));
     }
 }
---
+++ Day4/segregateosand1s.java
@@ -0,0 +1,26 @@
+package Day4;
+
+public class segregateosand1s{
+    public static void main(String[] args) {
+        int[] arr= {0,0,1,0,1,0};
+        int n=arr.length;
+
+       int i=0;
+       int j=n-1;
+       while(i<j){
+        if(arr[i]==0){
+            i++;
+        }
+        else {
+            int temp = arr[i];
+            arr[i] =arr[j];
+            arr[j]=temp;
+            j--;
+        }
+       }
+       for(int ele: arr){
+        System.out.print(ele+" ");
+       }
+       
+    }
+}
