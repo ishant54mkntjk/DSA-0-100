44d5134e9f74e9674b49c93558010d3dcfcc4d4f
Ishant Yadav <160306018+ishant54mkntjk@users.noreply.github.com>
2026-08-18 01:40:19 +0530

    Time: 2 ms (76.19%), Space: 42.4 MB (99.74%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        // Hashmap<Character,Integer> Freq =new Hashap<>();
+
+        // for(int i=0;i<s.length();i++){
+        //     freq[s.charAt(i)-'a']++;
+        // }
+        //  int res =0;
+        //  boolean hasodd=false;
+        // for(int i=0;i<freq;i++){
+        //     int val =freq[i];
+        //     if(val%2==0){
+        //        res +=val;
+        //     }
+        //     else{
+        //         res +=val -1;
+        //         hasodd=true;
+        //     }
+        // }
+        // if(hasodd) res++;
+
+        int n =s.length();
+        int freq[] =new int[52];
+        for (char ch : s.toCharArray()) {
+            if (ch >= 'a' && ch <= 'z') {
+                freq[ch - 'a']++;
+            } else {
+                freq[ch - 'A' + 26]++;
+            }
+        }
+        int res=0;
+        boolean hasodd=false;
+        for(int i=0;i<52;i++){
+            int val =freq[i];
+            if(val%2==0){
+               res +=val;
+            }
+            else{
+                res +=val -1;
+                hasodd=true;
+            }
+        }
+        if(hasodd) res++;
+        return res;
+    }
+}
