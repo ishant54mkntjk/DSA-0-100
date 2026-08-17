1f5d54f6cb5b1c6ddf68f051eea6dfa70ba74320
Ishant Yadav <160306018+ishant54mkntjk@users.noreply.github.com>
2026-08-18 01:50:23 +0530

    Time: 7 ms (49.92%), Space: 43.1 MB (58.05%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,13 +1,36 @@
 class Solution {
     public int longestPalindrome(String s) {
-        // Hashmap<Character,Integer> Freq =new Hashap<>();
+        HashMap<Character,Integer> freq =new HashMap<>();
 
-        // for(int i=0;i<s.length();i++){
-        //     freq[s.charAt(i)-'a']++;
+        for(char ch :s.toCharArray()){
+           freq.put(ch, freq.getOrDefault(ch,0)+ 1 );
+        }
+         int res =0;
+         boolean hasodd=false;
+        for(int val:freq.values()){
+            
+            if(val%2==0){
+               res +=val;
+            }
+            else{
+                res +=val -1;
+                hasodd=true;
+            }
+        }
+        if(hasodd) res++;
+
+        // int n =s.length();
+        // int freq[] =new int[52];
+        // for (char ch : s.toCharArray()) {
+        //     if (ch >= 'a' && ch <= 'z') {
+        //         freq[ch - 'a']++;
+        //     } else {
+        //         freq[ch - 'A' + 26]++;
+        //     }
         // }
-        //  int res =0;
-        //  boolean hasodd=false;
-        // for(int i=0;i<freq;i++){
+        // int res=0;
+        // boolean hasodd=false;
+        // for(int i=0;i<52;i++){
         //     int val =freq[i];
         //     if(val%2==0){
         //        res +=val;
@@ -18,29 +41,6 @@ class Solution {
         //     }
         // }
         // if(hasodd) res++;
-
-        int n =s.length();
-        int freq[] =new int[52];
-        for (char ch : s.toCharArray()) {
-            if (ch >= 'a' && ch <= 'z') {
-                freq[ch - 'a']++;
-            } else {
-                freq[ch - 'A' + 26]++;
-            }
-        }
-        int res=0;
-        boolean hasodd=false;
-        for(int i=0;i<52;i++){
-            int val =freq[i];
-            if(val%2==0){
-               res +=val;
-            }
-            else{
-                res +=val -1;
-                hasodd=true;
-            }
-        }
-        if(hasodd) res++;
         return res;
     }
 }
